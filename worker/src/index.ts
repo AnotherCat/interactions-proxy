2d06abc41f1b4cc115bfb235a9760d1766ff0feb
Sam <anothercat1259@gmail.com>
2021-09-07 17:10:38 +1200

    feat: add error handling for internal requests

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,21 +1,37 @@
 import { handleRequest } from './handler'
-import { InvalidRequest, MissingPermissions, ReturnedError } from './errors'
+import {
+  InternalRequestError,
+  InvalidRequest,
+  MissingPermissions,
+  ReturnedError,
+} from './errors'
 import { MessageFlags } from 'discord-api-types/v9'
 
 addEventListener('fetch', (event) => {
   event.respondWith(
-    handleRequest(event).catch(function (e) {
-      if (e instanceof InvalidRequest) {
-        return new Response(e.message, { status: 400 })
+    handleRequest(event).catch(function (error) {
+      if (error instanceof InvalidRequest) {
+        return new Response(error.message, { status: 400 })
       } else if (
-        e instanceof MissingPermissions ||
-        e instanceof ReturnedError
+        error instanceof MissingPermissions ||
+        error instanceof ReturnedError ||
+        error instanceof InternalRequestError
       ) {
+        let message: string
+        if (
+          error instanceof MissingPermissions ||
+          error instanceof ReturnedError
+        ) {
+          message = error.message
+        } else {
+          message = `An error occurred when making a request. Status: ${error.response.status} Error: ${error.message}. \nPlease report this so it can be fixed`
+        }
+
         return new Response(
           JSON.stringify({
             type: 4,
             data: {
-              content: e.message,
+              content: message,
               flags: MessageFlags.Ephemeral,
             },
           }),
@@ -26,7 +42,7 @@ addEventListener('fetch', (event) => {
           },
         )
       } else {
-        return new Response('internal error', { status: 500 })
+        throw error
       }
     }),
   )
