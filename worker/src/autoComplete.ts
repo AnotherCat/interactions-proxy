f2b4c422c493e8b36a0e03b1abb85aaa3098bf0e
Sam <anothercat1259@gmail.com>
2021-09-23 21:35:26 +1200

    feat: Autocomplete for identifiers

---
+++ worker/src/autoComplete.ts
@@ -0,0 +1,112 @@
+import { APIUser, ApplicationCommandOptionType } from 'discord-api-types'
+import { InvalidRequest } from './errors'
+import { listFronts } from './fronts'
+
+const handleIdentifier = async (
+  option: AutocompleteOption,
+  user: APIUser,
+): Promise<Record<string, any>> => {
+  // Promise<APIInteractionResponse> => { TODO update this when discord-api-types releases
+  const ids = await listFronts(user.id)
+  const mappedIds = ids
+    ? ids.map((id) => {
+        return {
+          name: id,
+          value: id,
+        }
+      })
+    : null
+
+  const IdsToReturn = mappedIds?.filter(
+    (id) => option.value === '' || id.value.startsWith(option.value),
+  ) // TODO - fix this once discord-api-types is out
+  console.log(
+    JSON.stringify({
+      type: 8,
+      data: {
+        choices: IdsToReturn,
+      },
+    }),
+  )
+  return {
+    type: 8,
+    data: {
+      choices: IdsToReturn,
+    },
+  }
+}
+
+interface AutocompleteOption {
+  type: ApplicationCommandOptionType.String
+  name: string
+  value: any
+  focused: boolean
+}
+
+const handleManageFrontsAutocomplete = async (
+  interaction: any,
+): Promise<Record<string, any>> => {
+  // Promise<APIInteractionResponse> => { TODO update this when discord-api-types releases
+  if (
+    !interaction.data.options ||
+    interaction.data.options[0].type !== ApplicationCommandOptionType.Subcommand
+  ) {
+    throw new InvalidRequest(
+      'Incorrect options on "manage-fronts" autocomplete',
+    )
+  }
+  if (
+    interaction.data.options[0].name !== 'get' &&
+    interaction.data.options[0].name !== 'delete'
+  ) {
+    throw new InvalidRequest(
+      "Invalid subcommand for 'manage-fronts' autocomplete",
+    )
+  }
+  console.log(JSON.stringify(interaction.data.options[0].options))
+  const focusedOptions = interaction.data.options[0].options.filter(
+    (option: AutocompleteOption) => option.focused,
+  )
+  console.log(JSON.stringify(focusedOptions))
+  if (focusedOptions.length > 1 || focusedOptions.length === 0) {
+    throw new InvalidRequest('There must be one focused option!')
+  }
+  return await handleIdentifier(
+    focusedOptions[0] as AutocompleteOption,
+    (interaction.user || interaction.member.user)!,
+  )
+}
+
+const handleProxyAutocomplete = async (
+  interaction: any,
+): Promise<Record<string, any>> => {
+  // Promise<APIInteractionResponse> => { TODO update this when discord-api-types releases
+  console.log(JSON.stringify(interaction.data.options))
+  const focusedOptions = interaction.data.options.filter(
+    (option: AutocompleteOption) => option.focused,
+  )
+  console.log(JSON.stringify(focusedOptions))
+  if (focusedOptions.length > 1 || focusedOptions.length === 0) {
+    throw new InvalidRequest('There must be one focused option!')
+  }
+  return await handleIdentifier(
+    focusedOptions[0] as AutocompleteOption,
+    (interaction.user || interaction.member.user)!,
+  )
+}
+
+const handleAutocomplete = async (
+  interaction: any,
+): Promise<Record<string, any>> => {
+  // Promise<APIInteractionResponse> => { TODO update this when discord-api-types releases
+  switch (interaction.data.name) {
+    case 'manage-fronts':
+      return handleManageFrontsAutocomplete(interaction)
+    case 'proxy':
+      return handleProxyAutocomplete(interaction)
+    default:
+      throw new InvalidRequest('That application command was not found')
+  }
+}
+
+export { handleAutocomplete }
--- worker/src/handler.ts
+++ worker/src/handler.ts
@@ -1,9 +1,12 @@
 import {
+  APIApplicationCommandInteraction,
   APIInteraction,
   InteractionResponseType,
   InteractionType,
 } from 'discord-api-types/v9'
+import { handleAutocomplete } from './autoComplete'
 import { handleCommands } from './commands'
+import { listFronts } from './fronts'
 import { verify } from './security'
 
 export async function handleRequest(event: FetchEvent): Promise<Response> {
@@ -16,6 +19,7 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
     return new Response('invalid request', { status: 401 })
   }
   const interaction = (await request.json()) as APIInteraction
+  console.log(JSON.stringify(interaction))
   /*
   return new Response(
     JSON.stringify({ type: InteractionResponseType.ChannelMessageWithSource, data: {content: JSON.stringify(interaction)} }),
@@ -25,7 +29,9 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
   );
   */
 
-  switch (interaction.type) {
+  switch (
+    interaction.type as number // TODO - remove this once discord-api-types adds autocomplete
+  ) {
     case InteractionType.Ping:
       return new Response(
         JSON.stringify({ type: InteractionResponseType.Pong }),
@@ -35,11 +41,22 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
       )
     case InteractionType.ApplicationCommand:
       return new Response(
-        JSON.stringify(await handleCommands(interaction, event)),
+        JSON.stringify(
+          await handleCommands(
+            interaction as APIApplicationCommandInteraction,
+            event,
+          ),
+        ), // TODO - remove this once discord-api-types adds autocomplete
         {
           headers: { 'Content-Type': 'application/json' },
         },
       )
+    case 4:
+      const resp = await handleAutocomplete(interaction)
+      console.log(resp)
+      return new Response(JSON.stringify(resp), {
+        headers: { 'Content-Type': 'application/json' },
+      })
     default:
       return new Response('invalid request', { status: 400 })
   }
